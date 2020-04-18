910c7cd1290f0c9dc7f34d61f3e127956e67b2eb
AbdulrhmanMohammed <a.abdeldaiem@dvit.me>
2020-04-18 16:59:36 +0200

    Return Order

--- models/return_voucher.py
+++ models/return_voucher.py
@@ -10,16 +10,38 @@ class ReturnVoucher(models.Model):
     date = fields.Date(string="Date", default=fields.Date.context_today)
     sale_id = fields.Many2one(comodel_name="sale.order", string="Sale Order")
     sale_date = fields.Datetime(string="Date", related='sale_id.date_order')
-    partner_id = fields.Many2one(related='sale_id.partner_id', readonly="1")
+    partner_id = fields.Many2one('res.partner', 'Customer')
     customer_ref = fields.Char(readonly="1")
-    delivery_number = fields.Char(string="delivery number", )
+    delivery_id = fields.Many2one(comodel_name="stock.picking", string="delivery number", required=False, )
     delivery_date = fields.Date(string="Date")
     reason_id = fields.Many2one(comodel_name="return.order", string="reason to return")
+    voucher_line_ids = fields.One2many(comodel_name="return.voucher.line", inverse_name="return_id")
     state = fields.Selection([
         ('draft', 'Draft RFO'),
         ('reviewed', 'Reviewed'),
         ('approve', 'return order'),
     ], 'Order Status', default='draft', copy=False, readonly=True)
+
+    @api.onchange('partner_id','sale_id')
+    def _onchange_partner_id(self):
+
+        for rec in self:
+            if rec.partner_id:
+                sale_ids = self.env['sale.order'].search([('partner_id', '=', rec.partner_id.id),
+                                                          ('state', 'not in', ['draft', 'cancel'])])
+                if sale_ids:
+                    return {'domain': {'sale_id': [('id', 'in', sale_ids.ids)]}}
+                else:
+                    return {'domain': {'sale_id': [('id', '=', False)]}}
+            else:
+                return {'domain': {'sale_id': [('id', '=', False)]}}
+
+    @api.onchange('sale_id', 'delivery_id')
+    def _onchange_sale_id(self):
+        if self.sale_id:
+            return {'domain': {'delivery_id': [('id', 'in', self.sale_id.picking_ids.ids)]}}
+        else:
+            return {'domain': {'delivery_id': [('id', 'in', False)]}}
 
     def action_reviewed(self):
         self.state = 'reviewed'
@@ -32,11 +54,6 @@ class ReturnVoucher(models.Model):
 
     def action_draft(self):
         self.state = 'draft'
-
-    @api.onchange('sale_id')
-    def _onchange_sale_id(self):
-        for rec in self:
-            rec.customer_ref = rec.sale_id.partner_id.code
 
     def random_number(self, n):
         range_start = 10 ** (n - 1)
@@ -54,3 +71,25 @@ class ReturnVoucher(models.Model):
     def create(self, values):
         values['name'] = self.check_voucher_code(str(self.random_number(4)))
         return super(ReturnVoucher, self).create(values)
+
+
+class ReturnVoucherLine(models.Model):
+    _name = 'return.voucher.line'
+    return_id = fields.Many2one(comodel_name="return.voucher", string="Return", )
+    product_id = fields.Many2one(comodel_name="product.product", string="Product", required=True)
+    description = fields.Char(string="Description", )
+    qty = fields.Float(string="Quantity", )
+    uom_id = fields.Many2one(comodel_name="uom.uom", string="UoM", )
+    price_unit = fields.Float(string="Price", )
+    price_subtotal = fields.Float(string="Subtotal")
+
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+        product_ids = []
+        delivery = self.return_id.delivery_id
+        for line in delivery:
+            product_ids.append(line.product_id.id)
+        if product_ids:
+            return {'domain': {'product_id': [('id', 'in', product_ids)]}}
+        else:
+            return {'domain': {'product_id': [('id', 'in', False)]}}
