3e177f8d78c3229a4eea05b8cfc39d3e6b99c907
AbdulrhmanMohammed <a.abdeldaiem@dvit.me>
2020-04-18 21:36:35 +0200

    REWrite Code

---
+++ models/return_reason.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+from random import randint
+
+
+class ReturnReason(models.Model):
+    _name = 'return.reason'
+    _rec_name = 'eng_description'
+
+    name = fields.Char(string="code", )
+    eng_description = fields.Char(string="English Description", required=True)
+    ara_description = fields.Char(string="Arabic Description", required=True)
+    active = fields.Boolean(string="Active", )
+
+
+    def random_number(self, n):
+        range_start = 10 ** (n - 1)
+        range_end = (10 ** n) - 1
+        return randint(range_start, range_end)
+
+    def check_return_code(self, code):
+        return_ids = self.env['return.reason'].search([('name', '=', str(code))])
+        while len(return_ids) >= 1:
+            code = str(self.random_number(4))
+            self.check_return_code(str(code))
+        return code
+
+    @api.model
+    def create(self, values):
+        values['name'] = self.check_return_code(str(self.random_number(4)))
+        return super(ReturnOrder, self).create(values)
+
+
+
+
+
+
+
+
+
+
