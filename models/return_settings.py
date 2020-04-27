d6d9d2d57aa81ca54baeaf20b1f92e336d295d70
AbdulrhmanMohammed <a.abdeldaiem@dvit.me>
2020-04-27 12:37:55 +0200

    Return

--- models/return_settings.py
+++ models/return_settings.py
@@ -6,5 +6,5 @@ from odoo import api, fields, models, _
 
 class ReturnConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
-
+    return_reason = fields.Selection(string="", selection=[('1', '1'), ('2', '2'), ], required=False, )
     use_bridge_account = fields.Boolean(string="Use Bridge Account", config_parameter='base_setup.use_bridge_account', )
